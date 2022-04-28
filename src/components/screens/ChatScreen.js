ba1483fef2051c48e03d0b453d3a89a6cc736882
fatihgenc <gnc.fath@gmail.com>
2022-04-29 02:37:36 +0300

    add control

--- src/components/screens/ChatScreen.js
+++ src/components/screens/ChatScreen.js
@@ -16,6 +16,14 @@ const ChatScreen = () => {
       messageContent: 'Merhabalar!',
       messageEmotes: [],
     },
+    {
+      id: Date.now(),
+      sender: 'fatih',
+      sendingDate: getCurrentTime(),
+      messageContent:
+        "Why do we use it? It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
+      messageEmotes: [],
+    },
   ]);
 
   const messagesContainerRef = useChatScroll(messages);
@@ -33,7 +41,7 @@ const ChatScreen = () => {
 
   const sendMessage = (_) => {
     const messageContent = msgRef.current.value;
-    if (messageContent === 0) return;
+    if (messageContent.trim().length === 0) return;
     setMessages((oldMessages) => [
       ...oldMessages,
       {
